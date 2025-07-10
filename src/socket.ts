ac952c1fbabeb3ffdf87a6391aaf37c9fbae78da
Vital Ozierski <vital@ozerich.com>
2025-07-10 18:13:08 +0200

    frontend

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
 // Укажи свой адрес сервера
-const socket = io("https://trader.ozerich.com");
+const socket = io("https://trader.ozerich.com/api");
 
 export default socket;
