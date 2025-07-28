d1dbd7083b44895bdeadf9c12b9e9c61a1b4063f
Vital Ozierski <vital@ozerich.com>
2025-07-28 13:14:57 +0200

    fix action

--- src/categories.ts
+++ src/categories.ts
@@ -5,18 +5,10 @@ export const CATEGORIES = [
     {id: 'other', index: 1, label: 'Other'},
 ];
 
-if (window.innerWidth > 2000) {
+if (window.innerWidth > 2500) {
     CATEGORIES.push({
         id: 'other',
         index: 2,
         label: 'Other 2'
-    });
-}
-
-if (window.innerWidth > 2500) {
-    CATEGORIES.push({
-        id: 'other',
-        index: 3,
-        label: 'Other 3'
     });
 }
